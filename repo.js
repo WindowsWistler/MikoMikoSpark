0d3f554a088808d5ebe06d871ededb08c006dd35
ReimusTobaccoStick <86897047+WindowsWistler@users.noreply.github.com>
2025-10-10 19:13:33 -0500

    Add files via upload
    needed for repo

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "u/CustomerAlternative",
+    "id": "MikoMikoSpark",
+    "patches": {
+        "unpx19": "Removes the pixel sprites (if able) from Touhou 19.",
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/MikoMikoSpark/"
+    ],
+    "title": "Miko Miko Spark!"
+}
