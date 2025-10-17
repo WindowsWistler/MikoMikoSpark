72928cb5b26893d8b2bd59db1eb8dbcce2d4c96d
ReimusTobaccoStick <86897047+WindowsWistler@users.noreply.github.com>
2025-10-17 18:50:46 -0500

    Update patch.js

--- unpx19/patch.js
+++ unpx19/patch.js
@@ -3,11 +3,12 @@
     "servers": [
         "https://mirrors.thpatch.net/MikoMikoSpark/unpx19/"
     ],
-    "title": "A patch that replaces Touhou 19 sprites with non-pixel sprites (if possible), as pixel sprites shouldn't be in danmaku games."
+    "title": "A patch that replaces Touhou 19 sprites with non-pixel sprites (if possible), as pixel sprites shouldn't be in danmaku games.",
     "dependencies": [
     "nmlgc/base_tsa"
     ]
 }
+
 
 
 
