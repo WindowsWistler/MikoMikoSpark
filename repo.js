6f1eec6317a7a21e5e21e2a55cf82dee5cebac5f
ReimusTobaccoStick <86897047+WindowsWistler@users.noreply.github.com>
2025-10-18 10:43:36 -0500

    Modify patches in repo.js
    Updated the 'sese' patch description and added a new 'sese-bgm' patch.

--- repo.js
+++ repo.js
@@ -3,7 +3,8 @@
     "id": "MikoMikoSpark",
     "patches": {
         "unpx19": "Removes the pixel sprites (if able) from Touhou 19.",
-        "sese": "Replaces Ariya with Sese."
+        "sese": "Was Sese always in Touhou 20?",
+        "sese-bgm": "Music patch bundled with the sese patch.",
     },
     "servers": [
         "https://mirrors.thpatch.net/MikoMikoSpark/"
@@ -11,4 +12,5 @@
     "title": "Miko Miko Spark!"
 
 }
+
 
