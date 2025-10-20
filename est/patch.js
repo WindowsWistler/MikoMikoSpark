11668ba223715b958e510f13178439076da52fb2
WindowsWistler <bratholes@gmail.com>
2025-10-20 17:36:15 -0500

    added pacths

---
+++ est/patch.js
@@ -0,0 +1,14 @@
+{
+    "id": "est",
+    "servers": [
+        "https://mirrors.thpatch.net/MikoMikoSpark/est/"
+    ],
+    "title": "Touhou 6: Eastern Scarlet Teahouse.",
+    "dependencies": [
+    "nmlgc/base_tsa",
+    "MikoMikoSpark/bgm-old"
+    ]
+}
+
+
+
---
+++ yakumoran/patch.js
@@ -0,0 +1,16 @@
+{
+    "id": "yakumoran",
+    "servers": [
+        "https://mirrors.thpatch.net/MikoMikoSpark/yakumoran/"
+    ],
+    "title": "reemplaza todo con Ran jajaja",
+    "dependencies": [
+    "nmlgc/base_tsa"
+	"thpatch/lang_es"
+    ]
+}
+
+
+
+
+
