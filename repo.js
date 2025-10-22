f82b63e596a3e32fde688139814f3875f3b3c94b
WindowsWistler <bratholes@gmail.com>
2025-10-21 21:08:46 -0500

    repo.js

--- repo.js
+++ repo.js
@@ -5,9 +5,9 @@
         "unpx19": "Removes the pixel sprites (if able) from Touhou 19.",
         "sese": "Was Sese always in Touhou 20?",
         "sese-bgm": "Music patch bundled with the sese patch.",
-		"bgm-old": "Old BGM music."
-		"eosd-doll": "EoSD but with the Dolls in Pseudo Paradise tracks."
-		"est": "Touhou 6: Eastern Scarlet Teahouse."
+		"bgm-old": "Old BGM music.",
+		"eosd-doll": "EoSD but with the Dolls in Pseudo Paradise tracks.",
+		"est": "Touhou 6: Eastern Scarlet Teahouse.",
 		"yakumoran": "reemplaza todo con Ran jajaja"
     },
     "servers": [
