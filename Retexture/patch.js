3774d2f0fbb992e8c295e091cb99f5b89b1793bc
WindowsWistler <bratholes@gmail.com>
2025-10-22 17:57:50 -0500

    retexture

---
+++ Retexture/patch.js
@@ -0,0 +1,16 @@
+{
+    "id": "Retexture",
+    "servers": [
+        "https://mirrors.thpatch.net/MikoMikoSpark/Retexture/"
+    ],
+    "title": "Updates the sprites to the newest versions.",
+    "dependencies": [
+    "nmlgc/base_tsa",
+	"nmlgc/base_tasofro"
+    ]
+}
+
+
+
+
+
