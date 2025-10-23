d5a8bf278f34abb1827a5ea5c66a03a61b8260d3
WindowsWistler <bratholes@gmail.com>
2025-10-23 18:36:54 -0500

    stringstorm

---
+++ springstorm/patch.js
@@ -0,0 +1,16 @@
+{
+    "id": "stringstorm",
+    "servers": [
+        "https://mirrors.thpatch.net/MikoMikoSpark/stringstorm/"
+    ],
+    "title": "Replaces staff roll themes with the game-dedicated vocal songs by StringStorm.",
+    "dependencies": [
+    "nmlgc/base_tsa",
+	"nmlgc/base_tasofro"
+    ]
+}
+
+
+
+
+
