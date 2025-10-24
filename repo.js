521bd0fbc1112ee2e89f43cbbfa80d9393cd71f3
WindowsWistler <bratholes@gmail.com>
2025-10-24 17:40:13 -0500

    emergency fix!

--- repo.js
+++ repo.js
@@ -8,7 +8,10 @@
 		"bgm-old": "Old BGM music.",
 		"eosd-doll": "EoSD but with the Dolls in Pseudo Paradise tracks.",
 		"est": "Touhou 6: Eastern Scarlet Teahouse.",
-		"yakumoran": "reemplaza todo con Ran jajaja"
+		"yakumoran": "reemplaza todo con Ran jajaja",
+		"pcb-old": "Touhou 7: Perfect Classic Blossom.",
+		"Retexture": "Updates the sprites to the newest versions.",
+		"stringstorm": "Replaces staff roll themes with the game-dedicated vocal songs by StringStorm."
     },
     "servers": [
         "https://mirrors.thpatch.net/MikoMikoSpark/"
