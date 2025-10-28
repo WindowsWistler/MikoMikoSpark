4a51dc58a1dae40333b5c144199ce5142ef0c8ce
WindowsWistler <bratholes@gmail.com>
2025-10-28 17:13:29 -0500

    git

---
+++ sese/files.js
@@ -0,0 +1,17 @@
+{
+	"patch.js": 931180675,
+	"th20/face/enemy6/ename01.png": 707738554,
+	"th20/face/enemy6/face01an.png": 2110953490,
+	"th20/face/enemy6/face01bs.png": 4241329096,
+	"th20/face/enemy6/face01ct.png": 2019412037,
+	"th20/face/enemy6/face01dp.png": 829255774,
+	"th20/face/enemy6/face01hp.png": 4181755573,
+	"th20/face/enemy6/face01n2.png": 767371452,
+	"th20/face/enemy6/face01no.png": 2678335347,
+	"th20/face/enemy6/face01pr.png": 3776961288,
+	"th20/face/enemy6/face01sp.png": 4181755573,
+	"th20/face/enemy6/face01sw.png": 2715909173,
+	"th20/face/enemy6/template.context": 1896896773,
+	"th20/face/enemy6/template.png": 2678335347,
+	"th20/front/ename.png": 1610064177
+}
