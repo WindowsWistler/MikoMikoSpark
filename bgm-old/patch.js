6530c3f5f885ff8fbd4e62ef9981ae0c8585c780
WindowsWistler <bratholes@gmail.com>
2025-10-28 17:04:18 -0500

    git

--- bgm-old/patch.js
+++ bgm-old/patch.js
@@ -1,13 +1,10 @@
 {
-    "id": "bgm-old",
-    "servers": [
-        "https://raw.githubusercontent.com/WindowsWistler/MikoMikoSpark/master/bgm-old/"
-    ],
-    "title": "Old BGM music.",
-    "dependencies": [
-    "nmlgc/base_tsa"
-    ]
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "bgm-old",
+	"servers": [
+		"https://raw.githubusercontent.com/WindowsWistler/MikoMikoSpark/master/bgm-old/"
+	],
+	"title": "Old BGM music."
 }
-
-
-
--- repo.js
+++ repo.js
@@ -1,24 +1,21 @@
 {
-    "contact": "u/CustomerAlternative",
-    "id": "MikoMikoSpark",
-    "patches": {
-        "unpx19": "Removes the pixel sprites (if able) from Touhou 19.",
-        "sese": "Was Sese always in Touhou 20?",
-        "sese-bgm": "Music patch bundled with the sese patch.",
+	"contact": "u/CustomerAlternative",
+	"id": "MikoMikoSpark",
+	"patches": {
+		"Retexture": "Updates the sprites to the newest versions.",
 		"bgm-old": "Old BGM music.",
+		"defo": "reosd but if it was reversed",
 		"eosd-doll": "EoSD but with the Dolls in Pseudo Paradise tracks.",
 		"est": "Touhou 6: Eastern Scarlet Teahouse.",
-		"yakumoran": "reemplaza todo con Ran jajaja",
 		"pcb-old": "Touhou 7: Perfect Classic Blossom.",
-		"Retexture": "Updates the sprites to the newest versions.",
+		"sese": "Was Sese always in Touhou 20?",
+		"sese-bgm": "Music patch bundled with the sese patch.",
 		"stringstorm": "Replaces staff roll themes with the game-dedicated vocal songs by StringStorm.",
-		"defo": "reosd but if it was reversed"
-    },
-    "servers": [
-        "https://raw.githubusercontent.com/WindowsWistler/MikoMikoSpark/master/"
-    ],
-    "title": "Miko Miko Spark!"
-
+		"unpx19": "Removes the pixel sprites (if able) from Touhou 19.",
+		"yakumoran": "reemplaza todo con Ran jajaja"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/WindowsWistler/MikoMikoSpark/master/"
+	],
+	"title": "Miko Miko Spark!"
 }
-
-
