23b7df5d9c7dfd92b4b7f1ff3fe54784f5de6165
WindowsWistler <bratholes@gmail.com>
2025-11-17 19:41:37 -0600

    a touhou AU co-made by me and my friend ceci :3

---
+++ toAUhou/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "toAUhou",
+	"servers": [
+		"https://raw.githubusercontent.com/WindowsWistler/MikoMikoSpark/main/toAUhou/"
+	],
+	"title": "A Touhou AU if ZUN released the windows games a year earlier."
+}
