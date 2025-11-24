87c35791937aa2598c926a7041b4805160598492
WindowsWistler <bratholes@gmail.com>
2025-11-24 10:18:30 -0600

    fuck

---
+++ Retexture/th07/data/patch.js
@@ -0,0 +1,12 @@
+{
+    "id": "pcb-old",
+    "servers": [
+        "https://mirrors.thpatch.net/MikoMikoSpark/pcb-old/"
+    ],
+    "title": "Touhou 7: Perfect Cherry Blossom... but older.",
+    "dependencies": [
+    "nmlgc/base_tsa",
+	"MikoMikoSpark/bgm-old"
+    ]
+}
+
