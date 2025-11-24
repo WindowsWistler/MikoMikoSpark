99ad36b30456e1fdacaf4493b0912f79cef5fca4
WindowsWistler <bratholes@gmail.com>
2025-11-23 19:53:36 -0600

    a touhou AU co-made by me and my friend ceci :3

--- repo.js
+++ repo.js
@@ -11,6 +11,7 @@
 		"sese": "Was Sese always in Touhou 20?",
 		"sese-bgm": "Music patch bundled with the sese patch.",
 		"stringstorm": "Replaces staff roll themes with the game-dedicated vocal songs by StringStorm.",
+		"toAUhou": "A Touhou AU if ZUN released the windows games a year earlier.",
 		"unpx19": "Removes the pixel sprites (if able) from Touhou 19.",
 		"yakumoran": "reemplaza todo con Ran jajaja"
 	},
