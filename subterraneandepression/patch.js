52469ef8ee7adb0579a6dd315f0e34b9cba9df1f
WindowsWistler <bratholes@gmail.com>
2026-05-28 20:53:07 -0500

    WE ARE BACK BABYYY

---
+++ subterraneandepression/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+	],
+	"id": "subterraneandepression",
+	"servers": [
+		"https://raw.githubusercontent.com/WindowsWistler/MikoMikoSpark/main/subterraneandepression/"
+	],
+	"title": "It's awfully cold in Old Hell now, and no one knows the culprit..."
+}
